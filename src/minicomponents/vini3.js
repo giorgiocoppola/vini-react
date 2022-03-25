37b40f428126aea1a28b91fe12943c498af2f780
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-03-25 20:19:56 +0100

    Creazione dell'app-React dei vini

---
+++ src/minicomponents/vini3.js
@@ -0,0 +1,33 @@
+
+const vini3 = [
+        
+    {
+        id:7,
+        img: "https://www.cantinedellacorte.it/255-home_default/gaio-dei-colli-trevigiani-igt-6-btg-75-cl.jpg",
+        nome: "Gaio dei colli Trevigiani",
+        prezzo: 44,
+        max:3,
+
+     },
+
+     {
+        id:8,
+        img: "https://www.isentieridibacco.it/media/catalog/product/cache/190b16899e2bd1673bb62630ffecea25/v/a/valdobbiadene-prosecco-docg-magnum-150cl.jpg",
+        nome: "Prosecco superiore DOCG",
+        prezzo: 21.90,
+        max:2,
+
+     },
+
+     {
+        id:9,
+        img: "https://www.proseccoandcartizze.com/wp-content/uploads/2020/05/defaveri-proseccoros%C3%A8.jpg",
+        nome: "Spumante Rosa Extra Dry",
+        prezzo: 11,
+        max:4,
+
+     }
+
+];
+
+export default vini3;
